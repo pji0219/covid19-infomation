e47d31a3efd3ba828e22dee809c09f7152e74b7c
park jong ik <pji219@naver.com>
2021-06-25 10:12:19 +0900

    푸터 메세지 변경

--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className={styles.footer}>
       <p className={styles.message}>
-        코로나가 얼른 종식 되기를 바라며
+        코로나가 어서 종식 되기를 바라며
         <br />
         의료진분들과 국민 여러분을 응원 합니다.
         <br />
