25301b5ec395092edc756bfd55ece131e862c97d
park jong ik <pji219@naver.com>
2021-06-26 21:05:15 +0900

    개발 모드 배포 모드 환경 변수 설정

---
+++ server/config/production.js
@@ -0,0 +1,4 @@
+module.exports = {
+  NEWS_ID: process.env.NEWS_ID,
+  NEWS_PASSWORD: process.env.NEWS_PASSWORD
+}
--- server/routers/news-router.js
+++ server/routers/news-router.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const query = encodeURI('코로나');
 const request = require('request');
 require('dotenv').config();
+const key = require('../config/key');
 
 const options = {
   method: 'GET',
   url: `https://openapi.naver.com/v1/search/news.json?query=${query}&display=10&start=1&sort=sim`,
   headers: {
-    'X-Naver-Client-Id': process.env.NEWS_ID,
-    'X-Naver-Client-Secret': process.env.NEWS_PASSWORD
+    'X-Naver-Client-Id': `${key.NEWS_ID}`,
+    'X-Naver-Client-Secret': `${key.NEWS_PASSWORD}`
   }
 };
 
