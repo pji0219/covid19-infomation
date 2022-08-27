1d038b3eed03ad955ce6d206d8e8b3f41ea7dfd8
pji0219 <pji219@naver.com>
2022-08-28 02:07:49 +0900

    필요 없는 라우터 삭제

--- server/index.js
+++ server/index.js
@@ -2,32 +2,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const korData = require('./routers/kor-corona-router');
-const globalData = require('./routers/global-corona-router');
-const usaData = require('./routers/usa-corona-router');
-const jpData = require('./routers/jp-corona-router');
-const cnData = require('./routers/cn-corona-router');
 const news = require('./routers/news-router');
 
 // cors 해결
-app.use(cors({
-  origin: ['https://know-corona-info.netlify.app']
-}));
-
-// 국내 종합 코로나 정보 api
-app.use('/api/kor', korData);
-
-// 해외 종합 코로나 정보 api
-app.use('/api/global', globalData);
-
-// 미국 종합 코로나 정보 api
-app.use('/api/usa', usaData);
-
-// 일본 종합 코로나 정보 api
-app.use('/api/jp', jpData);
-
-//중국 종합 코로나 정보 api
-app.use('/api/cn', cnData);
+app.use(
+  cors({
+    origin: ['https://know-corona-info.netlify.app'],
+  })
+);
 
 // 네이버 뉴스 api
 app.use('/api/navernews', news);
--- server/routers/news-router.js
+++ server/routers/news-router.js
@@ -3,20 +3,20 @@ const router = express.Router();
 const query = encodeURI('코로나');
 const request = require('request');
 require('dotenv').config();
-const id = process.env.NEWS_ID;
-const password = process.env.NEWS_PASSWORD;
+const id = process.env.NAVER_CLIENT_ID;
+const password = process.env.NAVER_CLIENT_SECRET;
 
 const options = {
   method: 'GET',
   url: `https://openapi.naver.com/v1/search/news.json?query=${query}&display=10&start=1&sort=sim`,
   headers: {
     'X-Naver-Client-Id': `${id}`,
-    'X-Naver-Client-Secret': `${password}`
-  }
+    'X-Naver-Client-Secret': `${password}`,
+  },
 };
 
 // 네이버 뉴스 라우터
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   request(options, (error, response, body) => {
     if (error) throw new Error(error);
     res.send(body);
